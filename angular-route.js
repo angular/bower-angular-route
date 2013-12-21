02b4fc46370c98dcad8273bee581f4f511478381
jenkins <ci-bot@angularjs.org>
2013-12-20 16:36:48 -0800

    v1.2.7-build.2023+sha.870232b

--- angular-route.js
+++ angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.7-build.2022+sha.c31df32
+ * @license AngularJS v1.2.7-build.2023+sha.870232b
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
