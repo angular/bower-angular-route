2bcb90e7eba9b4840bc064ccd457b994f5b97e4c
jenkins <ci-bot@angularjs.org>
2014-01-02 13:16:16 -0800

    v1.2.7-build.2038+sha.50bf029

--- angular-route.js
+++ angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.7-build.2036+sha.e415e91
+ * @license AngularJS v1.2.7-build.2038+sha.50bf029
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
