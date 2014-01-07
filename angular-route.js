f374fc0440c4c0fec3123a523463844815d03600
jenkins <ci-bot@angularjs.org>
2014-01-06 17:07:39 -0800

    v1.2.8-build.2084+sha.53fd24f

--- angular-route.js
+++ angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.8-build.2083+sha.28cfd96
+ * @license AngularJS v1.2.8-build.2084+sha.53fd24f
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
