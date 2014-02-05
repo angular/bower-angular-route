eedeba630f4a55897879e7352eae84d50cf282ca
jenkins <ci-bot@angularjs.org>
2014-02-04 16:28:57 -0800

    v1.2.12-build.2219+sha.058842a

--- angular-route.js
+++ angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.12-build.2217+sha.5850e61
+ * @license AngularJS v1.2.12-build.2219+sha.058842a
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
