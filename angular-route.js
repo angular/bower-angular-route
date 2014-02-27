dc4057d931755391886975255bb4a52fd24f6634
jenkins <ci-bot@angularjs.org>
2014-02-27 02:11:43 -0800

    v1.2.14-build.2338+sha.27873ac

--- angular-route.js
+++ angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.14-build.2337+sha.214c65d
+ * @license AngularJS v1.2.14-build.2338+sha.27873ac
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
