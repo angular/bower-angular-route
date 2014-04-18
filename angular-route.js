8d9fa8f177de265393b4ef8f583bca5c7a25f50b
jenkins <ci-bot@angularjs.org>
2014-04-18 12:50:56 -0700

    v1.3.0-build.2609+sha.f0407d2

--- angular-route.js
+++ angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.17-build.117+sha.24f7999
+ * @license AngularJS v1.3.0-build.2609+sha.f0407d2
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -518,7 +518,7 @@ function $RouteProvider(){
 
               angular.forEach(locals, function(value, key) {
                 locals[key] = angular.isString(value) ?
-                    $injector.get(value) : $injector.invoke(value);
+                    $injector.get(value) : $injector.invoke(value, null, null, key);
               });
 
               if (angular.isDefined(template = next.template)) {
