a82a2673fdb2362b165b82ab93ac940203591236
jenkins <ci-bot@angularjs.org>
2014-05-31 16:05:17 -0700

    v1.2.17-build.223+sha.d18d5f5

--- angular-route.js
+++ angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.17-build.222+sha.d18d5f5
+ * @license AngularJS v1.2.17-build.223+sha.d18d5f5
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
