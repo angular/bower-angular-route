bc13543ed0a0f0f29dce77ad9ef976f41b718ce7
jenkins <ci-bot@angularjs.org>
2014-06-05 13:29:11 -0700

    v1.2.17-build.226+sha.b6388b3

--- angular-route.js
+++ angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.2.17-build.225+sha.9227a5d
+ * @license AngularJS v1.2.17-build.226+sha.b6388b3
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -27,7 +27,7 @@ var ngRouteModule = angular.module('ngRoute', ['ng']).
 /**
  * @ngdoc provider
  * @name $routeProvider
- * @function
+ * @kind function
  *
  * @description
  *
