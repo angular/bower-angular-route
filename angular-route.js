bd2d770ee85d52b6988a76e4509c35ecbabbe45e
jenkins <ci-bot@angularjs.org>
2014-08-29 15:58:43 -0700

    v1.3.0-build.3159+sha.2294880

--- angular-route.js
+++ angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-build.3158+sha.0604bb7
+ * @license AngularJS v1.3.0-build.3159+sha.2294880
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
