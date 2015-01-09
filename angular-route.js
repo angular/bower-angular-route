25a5765a5deacb2b8b6fa102391cd257c6d6992f
jenkins <ci-bot@angularjs.org>
2015-01-09 14:54:03 -0800

    v1.3.9-build.10+sha.26ee32e

--- angular-route.js
+++ angular-route.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.9-build.3765+sha.3353afb
+ * @license AngularJS v1.3.9-build.10+sha.26ee32e
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -612,8 +612,8 @@ function $RouteProvider() {
               return $q.all(locals);
             }
           }).
+          // after route change
           then(function(locals) {
-            // after route change
             if (nextRoute == $route.current) {
               if (nextRoute) {
                 nextRoute.locals = locals;
