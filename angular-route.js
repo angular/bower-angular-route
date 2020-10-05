83ed0d092a0b3c61b88f58a3118fc21d838362f3
Pete Bacon Darwin <pete@bacondarwin.com>
2020-10-05 14:26:11 +0100

    v1.8.1

--- angular-route.js
+++ angular-route.js
@@ -1,6 +1,6 @@
 /**
- * @license AngularJS v1.8.0
- * (c) 2010-2020 Google, Inc. http://angularjs.org
+ * @license AngularJS v1.8.1
+ * (c) 2010-2020 Google LLC. http://angularjs.org
  * License: MIT
  */
 (function(window, angular) {'use strict';
@@ -101,7 +101,7 @@ var noop;
 /* global -ngRouteModule */
 var ngRouteModule = angular.
   module('ngRoute', []).
-  info({ angularVersion: '1.8.0' }).
+  info({ angularVersion: '1.8.1' }).
   provider('$route', $RouteProvider).
   // Ensure `$route` will be instantiated in time to capture the initial `$locationChangeSuccess`
   // event (unless explicitly disabled). This is necessary in case `ngView` is included in an
